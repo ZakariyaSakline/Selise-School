28231aa01ed123fe9ca223d7dc02a0f4ef034118
ZakariyaSakline <zakariya.sakline@selise.ch>
2019-07-18 19:18:42 +0600

    add sort

--- Myproject/src/app/data-table/data-table.component.ts
+++ Myproject/src/app/data-table/data-table.component.ts
@@ -1,28 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild } from '@angular/core';
 import { ProductService } from '../product.service';
 import {MatTableDataSource} from '@angular/material/table';
+import {MatSort} from '@angular/material/sort';
 
 
 
 export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+
+  proEdit:string;
+  proDelete:string;
+  static:boolean;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
 
 
 @Component({
@@ -37,14 +26,19 @@ export class DataTableComponent implements OnInit {
 
   constructor(private productservice:ProductService) { }
 
-  ngOnInit() {
-  }
 
    jasonData:[]=this.productservice.getLocalStorageProduct();
 
-   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-   dataSource = new MatTableDataSource(ELEMENT_DATA);
+   
+   displayedColumns: string[] = ['proId','proImage', 'proName', 'proPrice', 'proEdit','proDelete'];
+   dataSource = new MatTableDataSource(this.jasonData);
  
+   @ViewChild(MatSort, {static: true}) sort: MatSort;
+
+   ngOnInit() {
+     this.dataSource.sort = this.sort;
+   }
+
    applyFilter(filterValue: string) {
      this.dataSource.filter = filterValue.trim().toLowerCase();
    }
